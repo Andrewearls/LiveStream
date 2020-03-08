9ef684a01205858af23399285b8644210f8906da
Andrew Earls <andrewearls@rocketmail.com>
2020-03-08 11:27:41 -0700

    toggle video container on click

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,111 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>LiveStream</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+
+        <!-- Scripts -->
+        <script src="https://kit.fontawesome.com/c6eca12015.js" crossorigin="anonymous"></script>
+
+        <!-- Styles -->
+        <style>
+            html, body {
+                background-color: #fff;
+                color: #636b6f;
+                font-family: 'Nunito', sans-serif;
+                font-weight: 200;
+                height: 100vh;
+                margin: 0;
+            }
+
+            p {
+                margin-bottom: 0px;
+            }
+
+            .full-height {
+                height: 100vh;
+            }
+
+            .flex-center {
+                align-items: center;
+                display: flex;
+                justify-content: center;
+            }
+
+            .position-ref {
+                position: relative;
+            }
+
+            .top-right {
+                position: absolute;
+                right: 10px;
+                top: 18px;
+            }
+
+            .content {
+                text-align: center;
+                border: 3px solid gray;
+                border-radius: 10px;
+                width: 725px;
+                height: 420px;
+            }
+
+            .title {
+                font-size: 84px;
+            }
+
+            .links > a {
+                color: #636b6f;
+                padding: 0 25px;
+                font-size: 13px;
+                font-weight: 600;
+                letter-spacing: .1rem;
+                text-decoration: none;
+                text-transform: uppercase;
+            }
+
+            .m-b-md {    
+                width: 50%;
+                height: 50%;
+                margin: auto;
+                align-content: center;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="flex-center position-ref full-height">
+            @if (Route::has('login'))
+                <div class="top-right links">
+                    @auth
+                        <a href="{{ url('/home') }}">Home</a>
+                    @else
+                        <a href="{{ route('login') }}">Login</a>
+
+                        @if (Route::has('register'))
+                            <a href="{{ route('register') }}">Register</a>
+                        @endif
+                    @endauth
+                </div>
+            @endif
+
+            <div class="content" id="video-container">
+                <div class="title m-b-md">
+                    <p>LiveStream</p>
+                    <i class="far fa-play-circle"></i>
+                </div>
+
+            </div>
+        </div>
+    </body>
+    <script type="text/javascript">
+        document.getElementById("video-container").addEventListener("click", function (){
+            document.getElementById("video-container").innerHTML = "javascript";
+        });
+        
+    </script>
+</html>
