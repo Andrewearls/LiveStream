02060620c9c5b8b4a1cc4de16235d14180ec0d63
Andrew Earls <andrewearls@rocketmail.com>
2020-03-08 18:39:11 -0700

    using camera

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -27,6 +27,10 @@
                 margin-bottom: 0px;
             }
 
+            video {
+                width: 100%;
+            }
+
             .full-height {
                 height: 100vh;
             }
@@ -49,10 +53,11 @@
 
             .content {
                 text-align: center;
-                border: 3px solid gray;
+                border: 5px solid gray;
                 border-radius: 10px;
                 width: 725px;
                 height: 420px;
+                overflow: hidden;
             }
 
             .title {
@@ -75,6 +80,14 @@
                 margin: auto;
                 align-content: center;
             }
+
+            .hide {
+                display: none;
+            }
+
+            .show {
+                display: block;
+            }
         </style>
     </head>
     <body>
@@ -94,17 +107,69 @@
             @endif
 
             <div class="content" id="video-container">
-                <div class="title m-b-md">
+                <div class="title m-b-md" id="pause-screen">
                     <p>LiveStream</p>
                     <i class="far fa-play-circle"></i>
+                    
                 </div>
-
+                <video class="hide"></video>
+            </div>
+            <div id="errorMsg">
+                
             </div>
         </div>
     </body>
     <script type="text/javascript">
-        document.getElementById("video-container").addEventListener("click", function (){
-            document.getElementById("video-container").innerHTML = "javascript";
+        'use strict';
+
+        // Put variables in global scope to make them available to the browser console.
+        var video = document.querySelector('video');
+        var constraints = window.constraints = {
+            audio: true,
+            video: true
+        };
+        var errorElement = document.querySelector('#errorMsg');
+
+        document.getElementById("pause-screen").addEventListener("click", function (){
+            navigator.mediaDevices.getUserMedia(constraints)
+            .then(function(stream) {
+                var videoTracks = stream.getVideoTracks();
+                console.log('Got stream with constraints:', constraints);
+                console.log('Using video device: ' + videoTracks[0].label);
+                stream.onremovetrack = function() {
+                    console.log('Stream ended');
+                };
+                // window.stream = stream; // make variable available to browser console
+                video.srcObject = stream;
+                video.onloadedmetadata = function (e) {
+                    video.play();
+                    console.log("playing");
+                }
+            })
+            .catch(function(error) {
+                if (error.name === 'ConstraintNotSatisfiedError') {
+                    errorMsg('The resolution ' + constraints.video.width.exact + 'x' +
+                        constraints.video.width.exact + ' px is not supported by your device.');
+                } else if (error.name === 'PermissionDeniedError') {
+                    errorMsg('Permissions have not been granted to use your camera and ' +
+                        'microphone, you need to allow the page access to your devices in ' +
+                        'order for the demo to work.');
+                }
+                errorMsg('getUserMedia error: ' + error.name, error);
+            });
+
+            function errorMsg(msg, error) {
+                errorElement.innerHTML += '<p>' + msg + '</p>';
+                if (typeof error !== 'undefined') {
+                    console.error(error);
+                }
+            }
+            
+            document.querySelector(".title").classList.add("hide");
+            video.classList.remove("hide");
+            video.classList.add("show");
+            // document.getElementById("video-container").innerHTML = video;
+            
         });
         
     </script>
